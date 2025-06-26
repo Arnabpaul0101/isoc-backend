bd1161599baeffacfd44203676755d57360925e7
Arnab Paul <159263669+Arnabpaul0101@users.noreply.github.com>
2025-06-27 00:07:25 +0530

    Update auth.js

--- routes/authroutes/auth.js
+++ routes/authroutes/auth.js
@@ -20,7 +20,7 @@ router.get(
   }),
   (req, res) => {
     // Successful login
-    res.redirect("http://www.ieeesoc.xyz/dashboard"); // replace with your frontend
+    res.redirect("https://www.ieeesoc.xyz/dashboard"); // replace with your frontend
   }
 );
 
@@ -34,7 +34,7 @@ router.get("/logout", (req, res) => {
         sameSite: "lax",
         secure: false, // set to true in production with HTTPS
       });
-      res.redirect("http://www.ieeesoc.xyz/repos"); // replace with your frontend
+      res.redirect("https://www.ieeesoc.xyz/repos"); // replace with your frontend
     });
   });
 });
